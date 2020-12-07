665a6b022a99ea98e686dfffc375ad817142ce5c
Piotr Olearczyk <picu63@gmail.com>
2020-12-07 13:49:07 +0100

    coins scripts

---
+++ Assets/Scripts/DestroyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void OnCollision2D(GameObject other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            Destroy(other);
+        }
+    }
+}
---
+++ Assets/Scripts/PickupCoin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCoin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other);
+            Debug.Log("Coin was picked up");
+        }
+    }
+}
